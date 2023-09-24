4e079468421f519263e18e797dde8248cb96f89d
Ali Haidar <141362314+AliBinKhalid123@users.noreply.github.com>
2023-09-24 20:07:42 +0500

    * Registrations Controller Modified for Custom Attributes * devise migration modified and schema for trackable functionality * Navbar modified

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   has_many :projects, through: :user_projects
   has_many :bugs
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,  :trackable
 
   enum user_type: { manager: 'manager', developer: 'developer', qa: 'qa' }
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_924_023_337) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_24_023337) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,11 @@ ActiveRecord::Schema[7.0].define(version: 20_230_924_023_337) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
