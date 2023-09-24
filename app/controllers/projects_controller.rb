d56afc81c3d1ee706c98aaf71f0a2c780bda2918
Ali Haidar <141362314+AliBinKhalid123@users.noreply.github.com>
2023-09-24 14:26:37 +0500

    Projects controller actions Added

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,62 @@
 class ProjectsController < ApplicationController
   def index
+    if current_user.user_type == "qa"
+      @projects = Project.all
+    else
+      @projects = current_user.projects
+      if @projects.empty?
+        flash.now[:notice] = "You don't have any projects. Click below to create a new project."
+        @create_project_button = true
+      end
+    end
   end
 
   def new
+    @project = Project.new
+    authorize @project
   end
 
   def create
+    @project = current_user.projects.build(project_params)
+    authorize @project
+    current_user.projects << @project
+    if @project.save
+      redirect_to projects_path, notice: 'Project created successfully.'
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    authorize @project
+    @project.bugs.destroy_all
+    if UserProject.where(project_id: params[:id]).destroy_all
+      @project.destroy
+      redirect_to projects_path, notice: 'Project deleted successfully.'
+    else
+      redirect_to projects_path, notice: 'Project not Deleted'
+    end
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+    authorize @project
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    authorize @project
+    if @project.update(project_params)
+      redirect_to root_path, notice: "Project updated Sccesfully"
+    else
+      redirect_to root_path, notice: "Project not Updated"
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description)
   end
 end
