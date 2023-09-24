6c37b80f35c4958e0c4ad2df9e14f1eb3ebd29ba
Ali Haidar <141362314+AliBinKhalid123@users.noreply.github.com>
2023-09-24 13:32:49 +0500

    * Home Controller created * Devise Gem and Devise Contoroller Scop Added * home/index View Added * Navbar Partial View Added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root 'home#index'
+  # devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
