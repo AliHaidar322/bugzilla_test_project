6bc380ab620a46415a412144085b27f8ce930c91
Ali Haidar <141362314+AliBinKhalid123@users.noreply.github.com>
2023-09-25 10:54:45 +0500

    Bug Model Modified for Active Storage

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -1,11 +1,16 @@
 class Bug < ApplicationRecord
+  has_one_attached :screenshot
   belongs_to :user
   belongs_to :project
 
   enum status: { initiated: 0, started: 1, completed: 2, resolved: 3 }
 
+  enum
+
   validates :title, presence: true
   validates :deadline, presence: true
   validates :bug_type, presence: true
   validates :status, presence: true
+  validates :screenshot, content_type: ['image/png', 'image/gif'],
+  size: { less_than: 5.megabytes }
 end
