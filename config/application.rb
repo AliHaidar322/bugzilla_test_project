a116f5a802eb24bcf9c0f8a76f3e8d83866d2485
Ali Haidar <141362314+AliBinKhalid123@users.noreply.github.com>
2023-10-10 13:46:03 +0500

    Changes made for test environment setup

--- config/application.rb
+++ config/application.rb
@@ -7,8 +7,5 @@ Bundler.require(*Rails.groups)
 module Bugzilla
   class Application < Rails::Application
     config.load_defaults 7.0
-    config.generators do |g|
-      g.factory_bot factory: :factory, dir: 'spec/factories'
-    end
   end
 end
